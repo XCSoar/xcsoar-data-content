e0f236dc2a1794608cb5b06670d0e6dc553b62b0
Colin Sindle <csindle@gmail.com>
2021-12-22 18:39:33 +0200

    feat: Switch to HTTPS.

--- script/build/repository.py
+++ script/build/repository.py
@@ -105,7 +105,7 @@ if __name__ == "__main__":
     source_dir = root_dir / Path("source")
     remote_dir = root_dir / Path("remote")
 
-    base_url = "http://download.xcsoar.org/"
+    base_url = "https://download.xcsoar.org/"
 
     repo = ""
     repo += generate_content(data_dir=content_dir, url=base_url + "content/")
