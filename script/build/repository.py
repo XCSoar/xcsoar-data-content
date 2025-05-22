a8aaa55c7d73cd55404f88acdf434d1deeda37d2
Philipp Wollschlegel <folken@kabelsalat.ch>
2025-05-22 13:54:48 +0200

    openaip: switch to new naming for extended airspaces (fixup in repository)

--- script/build/repository.py
+++ script/build/repository.py
@@ -109,7 +109,7 @@ def generate_asp_openaip():
     contents = re.findall(r"<Contents>(.*?)</Contents>", openaip_index)
     for content in contents:
         key = re.search(r"<Key>(.*?)</Key>", content)
-        if key.group(1).__contains__("asp_extended.txt"):
+        if key.group(1).__contains__("asp_v2.txt"):
             size = re.search(r"<Size>(.*)</Size>", content)
             size = int(size.group(1))
             if size >= 384:
